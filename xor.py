6401ecdc1b376c911744bd3358419ef70288c3e2
林武震 <linwuzhen@linwuzhendeMacBook-Pro.local>
2016-01-20 11:53:23 +0800

    update xor.py

--- xor.py
+++ xor.py
@@ -1,3 +1,17 @@
 # -*- coding: utf8 -*-
+from itertools import izip, cycle
 
-print('xor')
+def getXorString(data, key):
+	return ''.join(chr(ord(_)^ord(__)) for _, __ in izip(data, cycle(key)))
+
+def main():
+	data = 'Hello world, my name is Lin.'
+	key = 'UCCU'
+	enc = getXorString(data, key)
+	print(enc)
+	open('enc', 'w').write(enc)
+	dec = getXorString(enc, key)
+	print(dec)
+
+if(__name__=='__main__'):
+	main()
